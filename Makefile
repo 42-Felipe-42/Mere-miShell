cfa6c56fcb767e9e43dd9e90d9245de74e538769
Philippe Langlois <plangloi@bess-f1r4s1.clusters.42paris.fr>
2024-05-31 18:30:57 +0200

    .h

---
+++ Makefile
@@ -0,0 +1,45 @@
+NAME        =   Mere-MiShell
+
+CC          =   cc
+
+FLAG        =   -Wall -Wextra -Werror -g3 
+
+LIBFT_PATH  =   .libft
+
+LIBFT_FILE  =   libft.a
+
+LIBFT_LIB   =   $(LIBFT_PATH)/$(LIBFT_FILE)
+
+C_FILE      =  	main.c \
+				srcs/utils_bis.c \
+				srcs/utils.c \
+
+OBJS        =   $(C_FILE:.c=.o)
+
+.c.o:
+	@printf "\r\033[K[Mere-MiShell] \033[0;32mBuilding : $<\033[0m"
+	@$(CC) $(FLAG) -c $< -o $@
+
+all: $(NAME)
+
+$(LIBFT_LIB):
+	@make -C $(LIBFT_PATH)
+	
+$(NAME): $(LIBFT_LIB) $(OBJS)
+	@printf "\r\033[K[Mere-MiShell] \033[0;32mLinking...\033[0m"
+	@$(CC) $(OBJS) $(LIBFT_LIB) -o $(NAME) -lm
+	@printf "\r\033[K[Mere-MiShell] \033[0;32mDone!\033[0m\n"
+
+clean:
+	@make clean -sC $(LIBFT_PATH)
+	@rm -f $(OBJS)
+	@printf "[Mere-MiShell] \033[1;31mCleaned .o!\033[0m\n"
+
+fclean: clean 
+	@rm -f $(NAME) $(BONUS)
+	@make fclean -C $(LIBFT_PATH)
+	@printf "[Mere-MiShell] \033[1;31mCleaned all!\033[0m\n"
+
+re: fclean all
+
+.PHONY: all clean fclean re 
---
+++ minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plangloi <plangloi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 11:12:08 by plangloi          #+#    #+#             */
+/*   Updated: 2024/05/31 11:41:05 by plangloi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	MINISHELL_H
+#define MINISHELL_H
+
+#include "../.libft/libft.h"
+#include "../expander/expander.h"
+#include "../lexer/lexer.h"
+#include "../parser/parser.h"
+
+#endif
